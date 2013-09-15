e788eddcc845fc1940f2be96001eb2d96d96a74a
Guillaume Aubert <aubertg@cpan.org>
2013-09-14 22:57:00 -0400

    Clean up install process.
    This now skips some files which exist in the repo (like the license), but should not be symlinked.

--- install.sh
+++ install.sh
@@ -1,7 +1,30 @@
 #!/usr/bin/env bash
 
+# List of files which are part of the repository but should not be symlinked.
+IGNORED_FILES='
+	.git
+	LICENSE
+	README.md
+	install.sh
+'
+
+# Create a backup directory for the files we're going to replace.
+[ ! -d $HOME/.original ] && mkdir $HOME/.original
+
+# Create symlinks for all the files that exist in the repository.
 for file in $(ls $HOME/dot-files/); do
-	if [ ! -e "$HOME/.$file" ]; then
-		ln -sf $HOME/dot-files/$file $HOME/.$file
+	# Skip ignored files.
+	[[ $IGNORED_FILES =~ $file ]] && continue
+
+	# Remove the destination file if it is a symlink, otherwise archive it.
+	if [ -e $HOME/.$file ]; then
+		if [ -L $HOME/.$file ]; then
+			rm $HOME/.$file
+		else
+			mv $HOME/.$file $HOME/.original/.$file
+		fi
 	fi
+
+	# Symlink our file.
+	ln -sf $HOME/dot-files/$file $HOME/.$file
 done
