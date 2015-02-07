d7b98533cb5cbb6335394a13d77349dbc68c70cb
Guillaume Aubert <aubertg@cpan.org>
2015-02-07 00:37:37 -0800

    Double quote to prevent globbing and word splitting.

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ IGNORED_FILES='
 '
 
 # Create a backup directory for the files we're going to replace.
-[ ! -d $HOME/.original ] && mkdir $HOME/.original
+[ ! -d "$HOME/.original" ] && mkdir "$HOME/.original"
 
 # Create symlinks for all the files that exist in the repository.
-cd $HOME/dot-files/
+cd "$HOME/dot-files"
 for file in *; do
 	# Skip ignored files.
 	[[ $IGNORED_FILES =~ $file ]] && continue
@@ -25,21 +25,21 @@ for file in *; do
 	[[ "$file" =~ ^gitconfig-v ]] && continue
 
 	# Remove the destination file if it is a symlink, otherwise archive it.
-	if [ -e $HOME/.$file ]; then
-		if [ -L $HOME/.$file ]; then
-			rm $HOME/.$file
+	if [ -e "$HOME/.$file" ]; then
+		if [ -L "$HOME/.$file" ]; then
+			rm "$HOME/.$file"
 		else
-			mv $HOME/.$file $HOME/.original/.$file
+			mv "$HOME/.$file" "$HOME/.original/.$file"
 		fi
 	fi
 
 	# Symlink our file.
-	ln -sf $HOME/dot-files/$file $HOME/.$file
+	ln -sf "$HOME/dot-files/$file" "$HOME/.$file"
 done
 cd -
 
 # Special handling for .gitconfig, to account for different versions of git.
 if [ "$(git --version | cut -c13)" == 2 ]; then
-	rm $HOME/.gitconfig
+	rm "$HOME/.gitconfig"
 	ln -sf "$HOME/dot-files/gitconfig-v2.0.0" "$HOME/.gitconfig"
 fi
