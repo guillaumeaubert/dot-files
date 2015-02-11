0ec98fcf71c26fbb96bb0fe64bc02104cde89a6d
Guillaume Aubert <aubertg@cpan.org>
2015-02-10 19:11:09 -0800

    Show how PPI breaks down a Perl file.

--- bash_includes/functions.sh
+++ bash_includes/functions.sh
@@ -10,3 +10,13 @@ dig_all () {
   dig +nocmd $1 any +multiline +noall +answer
 }
 export -f dig_all
+
+# Detail visually how PPI breaks down a Perl document.
+ppi() {
+	perl -le '
+		use PPI::Document;
+		use PPI::Dumper;
+		my $ppi_document = PPI::Document->new("'$1'");
+		PPI::Dumper->new( $ppi_document )->print();
+	'
+}
