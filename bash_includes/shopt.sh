0585f6cf445b1b5ca5c9efbbff36d62a9d73c34a
Guillaume Aubert <aubertg@cpan.org>
2015-02-11 00:26:50 -0800

    Add a few useful bash 4 options.

--- bash_includes/shopt.sh
+++ bash_includes/shopt.sh
@@ -6,3 +6,9 @@ shopt -s histappend cmdhist
 # Checks the window size after each command and, if necessary, update the
 # values of the variables LINES and COLUMNS.
 shopt -s checkwinsize
+
+# Make 'cd **/d' enter './a/b/c/d' (bash 4 only).
+shopt -s autocd 2> /dev/null
+
+# Recursive globbing - ls **/*.txt (bash 4 only).
+shopt -s globstar 2> /dev/null
