6752eb417d1a6c3feb77e9dcf55f6d5eb30b1d89
Guillaume Aubert <aubertg@cpan.org>
2015-03-23 14:30:39 -0700

    Share history between shells.

--- bash_includes/prompt.sh
+++ bash_includes/prompt.sh
@@ -5,3 +5,7 @@ source ~/dot-files/bash_includes/git-prompt.sh
 export PS1='[\u@\h \W\[\e[32m\]$(__git_ps1 " (%s)")\[\e[m\]]\$ '
 export GIT_PS1_SHOWDIRTYSTATE=1     # * for unstaged changes, + for staged changes.
 export GIT_PS1_SHOWUNTRACKEDFILES=1 # % for untracked files.
+
+# Whenever displaying the prompt, write the previous line to disk so that new
+# shells get the history lines from all previous shells.
+export PROMPT_COMMAND='history -a'
