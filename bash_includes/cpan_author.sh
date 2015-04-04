28b57fab662055b9e017979e87c8ac77ef3c235b
Guillaume Aubert <aubertg@cpan.org>
2015-04-04 16:53:09 -0700

    Function to run commands on all the distributions found under ~/cpan/.

--- bash_includes/cpan_author.sh
+++ bash_includes/cpan_author.sh
@@ -32,3 +32,38 @@ alias distupload='cpan-upload --user AUBERTG --verbose'
 # This is particularly useful for new distributions, in order to populate the
 # lists of prereqs in Build.PL.
 alias distprereq="perl $HOME/dot-files/tools/list_distribution_prereqs.pl"
+
+# Function to run commands on all the distributions found under ~/cpan/.
+all_dists()
+{
+	# Store the original directory to restore it at the end of the run.
+	original_dir=$(pwd)
+
+	# The command to run on each distribution.
+	command=$1
+
+	# Retrieve all the directories under ~/cpan/.
+	dists_found=0
+	for dist in ~/cpan/*/; do
+		cd $dist
+
+		# Skip directories that are not a git repository.
+		if [ ! -d .git ]; then
+			continue
+		fi
+		dists_found=$(($dists_found+1))
+
+		# Run the command on the distribution's repository;
+		echo -e "\e[4;35m$dist\e[0m"
+		export DIST=$dist
+		eval $command
+		echo ""
+	done
+
+	# Restore the original directory.
+	cd $original_dir
+
+	# Print how many distributions were affected.
+	echo -e "\e[0;92m✔ $dists_found distributions visited.\e[0m"
+}
+export -f all_dists
