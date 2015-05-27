c2756cfdfbc781bf493139367218b15ff45e18b8
Guillaume Aubert <aubertg@cpan.org>
2015-05-26 21:45:33 -0700

    Fix SC2086.
    SC2086: Double quote to prevent globbing and word splitting.

--- bash_includes/functions.sh
+++ bash_includes/functions.sh
@@ -3,14 +3,14 @@
 # Find out the version of an installed Perl module.
 pmver()
 {
-	perl -M$1 -e "print \$$1::VERSION, \"\n\""
+	perl -M"$1" -e "print \$$1::VERSION, \"\n\""
 }
 export -f pmver
 
 # Full dig information.
 dig_all()
 {
-  dig +nocmd $1 any +multiline +noall +answer
+  dig +nocmd "$1" any +multiline +noall +answer
 }
 export -f dig_all
 
@@ -20,7 +20,7 @@ ppi()
 	perl -le '
 		use PPI::Document;
 		use PPI::Dumper;
-		my $ppi_document = PPI::Document->new("'$1'");
+		my $ppi_document = PPI::Document->new("'"$1"'");
 		PPI::Dumper->new( $ppi_document )->print();
 	'
 }
@@ -29,7 +29,7 @@ export -f ppi
 # Allow going up several directories easily.
 up()
 {
-	cd $(eval printf '../'%.0s {1..$1})
+	cd $(eval printf '../'%.0s {1.."$1"})
 	pwd
 }
 export -f up
@@ -37,9 +37,9 @@ export -f up
 # Shortcut to create and sync new git branches.
 branch()
 {
-	git checkout -b $1 && \
-	git push origin $1 && \
-	git branch --set-upstream-to=origin/$1 $1
+	git checkout -b "$1" && \
+	git push origin "$1" && \
+	git branch --set-upstream-to="origin/$1" "$1"
 }
 export -f branch
 
@@ -49,8 +49,8 @@ export -f branch
 tag()
 {
 	git push && \
-	git tag $1 && \
-	git push origin $1
+	git tag "$1" && \
+	git push origin "$1"
 }
 export -f tag
 
@@ -95,5 +95,5 @@ export -f c
 # Create Linux user passwords.
 linuxpass()
 {
-	echo -n $1 | makepasswd --crypt-md5 --clearfrom -
+	echo -n "$1" | makepasswd --crypt-md5 --clearfrom -
 }
