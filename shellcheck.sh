936b7610c9b9ea52a7107dfbfd539d092f1445b2
Guillaume Aubert <aubertg@cpan.org>
2015-05-26 20:34:25 -0700

    Customize bash linter.

---
+++ shellcheck.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Paths to ignore.
+shellcheck_skip="
+	./tools/git-churn.sh
+	./bash_includes/git-completion.sh
+	./bash_includes/git-prompt.sh
+	./vim/undo/*
+"
+
+# Define find_cmd() to exclude files from $shellcheck_skip.
+find_cmd() {
+	cmd="find . -type f -and \( -perm +111 -or -name '*.sh' \) $(find_prunes)"
+	for path in $shellcheck_skip; do
+		cmd="$cmd ! -path '$path'"
+	done
+	echo "$cmd"
+}
+
+# Protect find_cmd() to prevent ./build/build.sh from redefining it.
+readonly -f find_cmd
+
+# Run the linter.
+source ./build/build.sh
