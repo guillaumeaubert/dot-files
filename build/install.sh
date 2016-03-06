897c35187ad767d57249b912f0fee06e44778e27
Guillaume Aubert <aubertg@cpan.org>
2016-03-05 22:21:36 -0800

    Force specific shellcheck version.

---
+++ build/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -eo pipefail
+
+main() {
+	local filename="shellcheck_0.3.7-5_amd64.deb"
+	wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
+	sudo dpkg -i "$filename"
+}
+
+main
