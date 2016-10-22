924e35c246ba1e982926d31d7fc146246f420359
Guillaume Aubert <aubertg@cpan.org>
2016-10-22 14:55:06 -0700

    Upgrade shellcheck version for testing.

--- build/install.sh
+++ build/install.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 main() {
-	local filename="shellcheck_0.3.7-5_amd64.deb"
+	local filename="shellcheck_0.4.4-3_amd64.deb"
 	wget "http://ftp.debian.org/debian/pool/main/s/shellcheck/$filename"
 	sudo dpkg -i "$filename"
 }
