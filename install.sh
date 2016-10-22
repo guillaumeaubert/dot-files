c3fd393f27d7d3d1494e0b0f1a2ddb8408d6378f
Guillaume Aubert <aubertg@cpan.org>
2016-10-22 15:13:21 -0700

    Fix SC2103.
    Use a ( subshell ) to avoid having to cd back.

--- install.sh
+++ install.sh
@@ -13,30 +13,31 @@ IGNORED_FILES='
 [ ! -d "$HOME/.original" ] && mkdir "$HOME/.original"
 
 # Create symlinks for all the files that exist in the repository.
-cd "$HOME/dot-files"
-for file in *; do
-	# Skip ignored files.
-	[[ $IGNORED_FILES =~ $file ]] && continue
+(
+	cd "$HOME/dot-files"
+	for file in *; do
+		# Skip ignored files.
+		[[ $IGNORED_FILES =~ $file ]] && continue
 
-	# Skip .selected_editor unless the system is Ubuntu.
-	[[ -e /etc/lsb-release && "$file" = "selected_editor" ]] && continue
+		# Skip .selected_editor unless the system is Ubuntu.
+		[[ -e /etc/lsb-release && "$file" = "selected_editor" ]] && continue
 
-	# Skip git version-specific files.
-	[[ "$file" =~ ^gitconfig-v ]] && continue
+		# Skip git version-specific files.
+		[[ "$file" =~ ^gitconfig-v ]] && continue
 
-	# Remove the destination file if it is a symlink, otherwise archive it.
-	if [ -e "$HOME/.$file" ]; then
-		if [ -L "$HOME/.$file" ]; then
-			rm "$HOME/.$file"
-		else
-			mv "$HOME/.$file" "$HOME/.original/.$file"
+		# Remove the destination file if it is a symlink, otherwise archive it.
+		if [ -e "$HOME/.$file" ]; then
+			if [ -L "$HOME/.$file" ]; then
+				rm "$HOME/.$file"
+			else
+				mv "$HOME/.$file" "$HOME/.original/.$file"
+			fi
 		fi
-	fi
 
-	# Symlink our file.
-	ln -sf "$HOME/dot-files/$file" "$HOME/.$file"
-done
-cd -
+		# Symlink our file.
+		ln -sf "$HOME/dot-files/$file" "$HOME/.$file"
+	done
+)
 
 # Special handling for .gitconfig, to account for different versions of git.
 if [ "$(git --version | cut -c13)" == 2 ]; then
